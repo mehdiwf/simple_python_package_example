c9f250480f975279161d8d4d9f9d0fd3657c1ccd
Mehdi Fawaz <mehdi.fawaz@univ-lyon1.fr>
2024-11-28 11:29:44 +0100

    updated test script

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,10 +1,8 @@
-import example_package_mifz as em
+import example_package_mifz as e
 
-print("should print 2:")
-print(em.example1.example_addone.add_one(1))
-print("should print 4:")
-print(em.example2.addtwo.add_two(2))
-print("should print 1:")
-print(em.example1.sub_one.sub_one(2))
-# print(example.add_one(1))
-# print(em.example.add_one(1))
+# this should print "hello!"
+e.printmod.say_hello()
+
+two = e.mymath.addone.add_one(1)
+# this should print "2"
+print(two)
