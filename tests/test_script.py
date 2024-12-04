3aa6aab75b174e13610fa515870144a504ca7bdb
Mehdi Fawaz <mehdi.fawaz@univ-lyon1.fr>
2024-12-04 09:37:43 +0100

    added test for My2DVector

--- tests/test_script.py
+++ tests/test_script.py
@@ -6,3 +6,11 @@ e.printmod.say_hello()
 two = e.mymath.addone.add_one(1)
 # this should print "2"
 print(two)
+
+v = e.my2Dvec.My2DVector(2, 0)
+# should print "vector: x= 2 , y= 0"
+v.show()
+
+v.normalize()
+# should print "vector: x= 1.0 , y= 0.0"
+v.show()
